1f3f7657c0d0e3ffb46909a3cc2a2273f8091be0
Marko Janic <marko.janic@hotmail.ch>
2024-02-03 16:04:38 +0100

    Create net_test_scicore.sh

---
+++ cifar10/net_test_scicore.sh
@@ -0,0 +1,11 @@
+# ===========================================================
+# 2d loss contours for ResNet-56
+# ===========================================================
+
+#mpirun -n 4 python plot_surface.py --x=-1:1:51 --y=-1:1:51 --model net \
+#--model_file cifar10/trained_nets/model_40epochs_lr1.0_gamma0.7_batchsize64/model.pt \
+#--mpi --cuda --dir_type weights --xignore biasbn --xnorm filter --yignore biasbn --ynorm filter
+
+python3 plot_surface.py --x=-1:1:51 --y=-1:1:51 --model net \
+--model_file cifar10/trained_nets/model_40epochs_lr1.0_gamma0.7_batchsize64/model.pt \
+--mpi --cuda --dir_type weights --xignore biasbn --xnorm filter --yignore biasbn --ynorm filter
