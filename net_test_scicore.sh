8c5f6841e25779b7070d7f57fd1a36fb418230d2
Marko Janic <marko.janic@hotmail.ch>
2024-02-03 16:05:49 +0100

    Update net_test_scicore.sh

--- net_test_scicore.sh
+++ net_test_scicore.sh
@@ -7,5 +7,5 @@
 #--mpi --cuda --dir_type weights --xignore biasbn --xnorm filter --yignore biasbn --ynorm filter
 
 python3 plot_surface.py --x=-1:1:51 --y=-1:1:51 --model net \
---model_file cifar10/trained_nets/model_40epochs_lr1.0_gamma0.7_batchsize64/model.pt \
+--model_file cifar10/trained_nets/model_scicore/model.pt \
 --mpi --cuda --dir_type weights --xignore biasbn --xnorm filter --yignore biasbn --ynorm filter
